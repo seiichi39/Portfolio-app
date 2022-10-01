f480ffd4f03290a8dae9c0447707aca858ac008c
seiichi39 <jhnnw708@gmail.com>
2022-10-01 22:34:36 +0900

    “パスワード再設定実装_本番環境②”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,14 +81,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://serene-sea-44230.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
-    :authentication => :plain,
-  }
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => 'tenniscourt110@gmail.com',
+    :password => 'dyuvqqpzfqxrcbgf',
+    :authentication => :plain
+  }  
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
