e0a40b6ddc525701ec726ff72e2b18065c6c93c5
seiichi39 <jhnnw708@gmail.com>
2022-10-01 22:23:46 +0900

    “パスワード再設定実装_本番環境”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,19 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-   # mailer setting
-   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://serene-sea-44230.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
+    :authentication => :plain,
+  }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
